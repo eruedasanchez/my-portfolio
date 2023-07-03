9513fe45a62354d641c438b419dfb0e5a68b8ea4
eruedasanchez <ezequiel.ruedasanchez@gmail.com>
2023-07-02 21:56:57 -0300

    Apply styles to contact section

--- js/main.js
+++ js/main.js
@@ -85,3 +85,42 @@ const filterFunc = (selectedValue) => {
         }
     }
 }
+
+// contact form variables
+const form = document.querySelector("[data-form]");
+const formInputs = document.querySelectorAll("[data-form-input]");
+const formBtn = document.querySelector("[data-form-btn]");
+
+// add events to all form input field
+
+for(let i=0; i < formInputs.length; i++){
+    formInputs[i].addEventListener("input", () => {
+        if(form.checkValidity()){
+            formBtn.removeAttribute("disabled");
+        } else {
+            formBtn.setAttribute("disabled", "");
+        }
+    });
+}
+
+// page navigation variables 
+const navigationLinks = document.querySelectorAll("[data-nav-link]");
+const pages = document.querySelectorAll("[data-page]");
+
+// add event to all nav link
+
+for(let i=0; i < navigationLinks.length; i++){
+    navigationLinks[i].addEventListener("click", () => {
+        for(let j=0; i < pages.length; j++){
+            if(this.innerHTML.toLowerCase() === pages[j].dataset.page){
+                pages[j].classList.add("active");
+                navigationLinks[j].classList.add("active");
+                window.scrollTo(0,0);
+            } else {
+                pages[j].classList.remove("active");
+                navigationLinks[j].classList.add("active");
+            }
+        }
+    });
+}
+
