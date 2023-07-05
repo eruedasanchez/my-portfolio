356d0cb77109261885601673a1820e6b69ef599d
eruedasanchez <ezequiel.ruedasanchez@gmail.com>
2023-07-05 12:31:30 -0300

    Delete classes, apply nesting and mixins to blog

--- js/main.js
+++ js/main.js
@@ -11,25 +11,25 @@ sidebarBtn.addEventListener('click', () => {
 });
 
 
-let blogPostsList = document.querySelector('.blog-posts-list'); 
+let blogPostsList = document.getElementById('blog-posts-list'); 
 
 const loadBlogList = (array) => {
     blogPostsList.innerHTML = '';
     array.forEach((blog) => {
         blogPostsList.innerHTML += `
-            <li class="blog-post-item">
+            <li>
                 <a href="">
-                    <figure class="blog-banner-box">
+                    <figure>
                         <img src="${blog.image}" alt="Design conferences in 2023" loading="lazy">
                     </figure>
                     <div class="blog-content">
                         <div class="blog-meta">
-                            <p class="blog-category">Design</p>
+                            <p>Design</p>
                             <span class="dot"></span>
                             <time datetime="2023-02-23">Feb 23, 2023</time>
                         </div>
-                        <h3 class="h3 blog-item-title">${blog.title}</h3>
-                        <p class="blog-text">${blog.description}</p>
+                        <h3 class="h3">${blog.title}</h3>
+                        <p>${blog.description}</p>
                     </div>
                 </a>
             </li>
